6bc9b89a91033ce572b4a8875797e031b5fcffff
MatthewDerzay <mderzay@wisc.edu>
2019-02-05 13:43:30 +0100

    Item Handler

---
+++ Assets/Scripts/ItemHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemHandler : MonoBehaviour
+{
+    GameObject pickedItem;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Item"))
+        {
+            if (pickedItem == null)
+            {
+                pickedItem = other.gameObject;
+                pickedItem.transform.SetParent(transform);
+                pickedItem.transform.position = transform.position + Vector3.right * 0.5f;
+            }
+        }
+    }
+}
